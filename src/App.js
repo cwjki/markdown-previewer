b9e47d7b13b4be47a491fe13accccc007ac494fd
cwjki <cwjki97@gmail.com>
2023-03-14 15:18:43 -0400

    adding bootstrap, and custom sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Editor from './components/Editor';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="container-fluid border">
+        <Editor />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Editor.js
@@ -0,0 +1,15 @@
+const Editor = () => {
+
+    return (
+        <div className="container-fluid vh-100">
+            <div className="row aling-items-center">
+                <div className="col-8 bg-primary bg-primary">
+                Editor Component test
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Editor;
