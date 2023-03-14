c4f5dce3c437fde6f7b7ace54b1d7ef33f41a427
cwjki <cwjki97@gmail.com>
2023-03-14 15:45:18 -0400

    working on the editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,27 @@
+import editorMessage from "../data/data";
+import { useState } from "react";
+
 const Editor = () => {
+    const [msg, setMsg] = useState(editorMessage);
+
 
     return (
-        <div className="container-fluid vh-100">
+        <div className="">
             <div className="row aling-items-center">
-                <div className="col-8 bg-primary bg-primary">
-                Editor Component test
+                <div className="col-6  mx-auto">
+                    <div className="card bg-warning text-primary">
+                        <div className="card-header">
+                            <p className="text-center text-primary fs-4">Editor</p>
+                        </div>
+                        <div className="card-body">
+                            <div className="">
+                            <label for="editorTextArea" className="form-label"></label>
+                            <textarea className="form-control" id="editorTextArea" rows="3">
+                                {msg}
+                            </textarea>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             
---
+++ src/data/data.js
@@ -0,0 +1,3 @@
+const editorMessage = "# Welcome to my React Markdown Previewer! ## This is a sub-heading... "
+
+export default editorMessage;
