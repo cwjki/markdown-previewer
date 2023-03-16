827412ad7ac24eb25a511e809a0095591c349356
cwjki <cwjki97@gmail.com>
2023-03-16 13:40:40 -0400

    adding some bold, italic and crossing text example

--- src/data/data.js
+++ src/data/data.js
@@ -10,6 +10,10 @@ const initialMsg = "" +
 "\nfunction anotherExample() { " +
 "\n  console.log('Hello world');" +
 "\n}\n```" +
+"You can also make text **bold**... whoa!" +
+"Or _italic_. " +
+"Or... wait for it... **_both!_**" +
+"And feel free to go crazy ~~crossing stuff out~~." +
 "\n\n My favorite search engine is [Duck Duck Go](https://duckduckgo.com)." + 
 "\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)" +
 ""
