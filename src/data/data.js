6728c9a3cb31aa990db9822139b3b4e15a836a99
cwjki <cwjki97@gmail.com>
2023-03-15 20:03:37 -0400

    creating the initial editor text example

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,14 @@
-const initialMsg = "# Welcome to my React Markdown Previewer! \n## This is a sub-heading... "
+const initialMsg = "" + 
+"# React Markdown Previewer!" +
+"\n## 👋 I'm cwjki." +
+"\n\n> #### The quarterly results look great!" +
+"\n\n Itemized lists look like:\n* this one\n* that one\n* the other one" +
+""
+
+
+
+
+
+
 
 export default initialMsg;
