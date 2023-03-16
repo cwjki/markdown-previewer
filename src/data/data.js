e58be4da352f03857ab9c4e8abd1d67d05327e7d
cwjki <cwjki97@gmail.com>
2023-03-16 12:40:04 -0400

    working on the initial msg

--- src/data/data.js
+++ src/data/data.js
@@ -1,15 +1,12 @@
 const initialMsg = "" + 
 "# 👋 I'm cwjki," +
-"\n a computer scientist and full stack developer." + 
+"\n* a computer scientist and full stack developer\n* bachelor's degree in Computer Science from the University of Havana\n* eager to keep learning and collaborate with great people" +
+"\n " + 
 "\n## This is a markdown previewer project " +
 "\n\n> #### The quarterly results look great!" +
-"\n\n Itemized lists look like:\n* this one\n* that one\n* the other one" +
+"\n\n Some of my skills:\n* this one\n* that one\n* the other one" +
+"My favorite search engine is [Duck Duck Go](https://duckduckgo.com)." + 
 ""
-
-
-
-
-
 
 
 export default initialMsg;
