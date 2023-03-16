105222eeb81c7bf20ae70af02f4a8585b4ff31ad
cwjki <cwjki97@gmail.com>
2023-03-16 13:37:33 -0400

    adding img

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,17 @@
 const initialMsg = "" + 
 "# 👋 I'm cwjki," +
-"\n* a computer scientist and full stack developer\n* bachelor's degree in Computer Science from the University of Havana\n* eager to keep learning and collaborate with great people" +
+"\n* a computer scientist and full stack developer\n* with a bachelor's degree in Computer Science from the University of Havana\n* eager to keep learning and collaborate with great people" +
 "\n " + 
-"\n## This is a markdown previewer project " +
-"\n\n> #### The quarterly results look great!" +
-"\n\n Some of my skills:\n* this one\n* that one\n* the other one" +
-"My favorite search engine is [Duck Duck Go](https://duckduckgo.com)." + 
+"\n## This is a markdown previewer project!" +
+"\n\n> #### Here are some examples:" +
+"\n\n Here's some code, `<div></div>`, between 2 backticks." +
+"\n```" + 
+"\n// this is a multi-line code\n" +
+"\nfunction anotherExample() { " +
+"\n  console.log('Hello world');" +
+"\n}\n```" +
+"\n\n My favorite search engine is [Duck Duck Go](https://duckduckgo.com)." + 
+"\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)" +
 ""
 
 
